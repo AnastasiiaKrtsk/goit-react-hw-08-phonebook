a0ad757e6f58e265ee73f99a258a703cdf716409
Dodge <93716715+AnastasiiaKrtsk@users.noreply.github.com>
2023-10-20 13:32:36 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from 'pages/Home';
 import Contacts from 'pages/Contacts';
 import Login from 'pages/Login';
